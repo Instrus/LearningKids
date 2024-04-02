3fa34093aef0b308fd085fd394e1e35fb549e618
ColtonPulliam <coltonjpulliam@gmail.com>
2024-04-02 16:51:19 -0500

    Added a working avatar screen that uses clickable arrows to cycle through avatars, hats, and body clothes
    Avatar Selection Completed

---
+++ LearningGame/Assets/_Scripts/Avatar_Screen/Customization.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//used https://www.youtube.com/watch?v=jPbdFOPkfy0&list=PLBIb_auVtBwBq9S1R-j4oL0HnlDh_rpLW&index=5
+//to create avatar screen prototype
+public class Customization : MonoBehaviour
+{
+   public Image part;
+   public Sprite[] options;
+   public int index;
+
+   void Update(){
+    for (int i = 0; i < options.Length; ++i){
+        if(i == index){
+            part.sprite = options[i];
+        }
+    }
+   }
+
+   public void Swap(){
+    if(index < options.Length - 1){
+        index++;
+    } else{
+        index = 0;
+    }
+   }
+
+}
