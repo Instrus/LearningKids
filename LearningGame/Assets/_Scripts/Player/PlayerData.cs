3afe34d83bdd62c3d6ec4174cd531f7cbb9ec608
Joe Linden <joeslinden@gmail.com>
2024-07-11 21:02:08 -0500

    feat: save all PlayerData, add contact info storage

--- LearningGame/Assets/_Scripts/Player/PlayerData.cs
+++ LearningGame/Assets/_Scripts/Player/PlayerData.cs
@@ -1,46 +1,47 @@
-using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerData : MonoBehaviour
 {
     [SerializeField] PlayerDataManager playerDM;
 
-    // Should remove all the default values. Create a new PlayerData and assign them default values within PDM.
+    // Default PlayerData members. Stored on local device.
+    [SerializeField] private int playerID = 1;
+    [SerializeField] private string username = "Blueberry";
+    [SerializeField] private int pin = 1234;
+    [SerializeField] private int score = 0;
+    [SerializeField] private int currency = 0;
+    [SerializeField] private int avatarIndex = 0;
+    [SerializeField] private int hatIndex = 0;
+    [SerializeField] private int shirtIndex = 0;
+    [SerializeField] private List<ContactInfo> contacts = new List<ContactInfo>();
 
-    // Holds data of player. Stored on local device.
-    private int playerID = 1; // Default testing PlayerID
-    private string username = "Blueberry"; // Default testing username
-    private int pin = 1234; // Default testing player PIN
-    private int score = 0; // initial player score
-    private int currency = 0; // initial player currency
-    // Avatar cosmetic indices
-    private int avatarIndex = 0;
-    private int hatIndex = 0;
-    private int shirtIndex = 0;
-    // potential: (Phone number / Home address)
+    // Save all serialized player data to a persistent JSON file.
+    public void Save() { playerDM.SavePlayerDataToFile(); }
 
     // Player ID
     public int GetPlayerID() { return playerID; }
-    public void SetPlayerID(int playerID) { this.playerID = playerID; }
+    public void SetPlayerID(int playerID) { this.playerID = playerID; Save(); }
 
     // Player username
     public string GetUsername() { return username; }
-    public void SetUsername(string userName) { this.username = userName; }
+    public void SetUsername(string userName) { this.username = userName; Save(); }
 
     // Player PIN
     public int GetPin() { return pin; }
-    public void SetPIN(int pin) { this.pin = pin; }
+    public void SetPIN(int pin) { this.pin = pin; Save(); }
 
     // Player currency
     public int GetCurrency() { return currency; }
-    public void SetCurrency(int currency) //Do not call. Call AddCurrency
-    { this.currency = currency; playerDM.SavePlayerDataToFile(); }  // Save the currency locally
-    public void AddCurrency(int currency) { SetCurrency(GetCurrency() + currency); } 
+    public void SetCurrency(int currency) // Do not call. Call AddCurrency
+    { this.currency = currency; Save(); }
+    public void AddCurrency(int currency) { SetCurrency(GetCurrency() + currency); }
 
     // Scores
     public int GetScore() { return score; }
-    public void SetScore(int score) //Do not call. Call AddScore
-    { this.score = score; playerDM.SavePlayerDataToFile(); } // Save the score locally
+    public void SetScore(int score) // Do not call. Call AddScore
+    { this.score = score; Save(); }
     public void AddScore(int score) { SetScore(GetScore() + score); }
 
     // Avatar cosmetics
@@ -50,6 +51,27 @@ public class PlayerData : MonoBehaviour
         this.avatarIndex = avatarIndex;
         this.hatIndex = hatIndex;
         this.shirtIndex = shirtIndex;
+        Save();
     }
-    
+
+    // Contact information
+    public List<ContactInfo> GetContacts() { return new List<ContactInfo>(contacts); }
+    public void AddContact(string name, string phoneNumber)
+    { contacts.Add(new ContactInfo(name, phoneNumber)); Save(); }
+    public void RemoveContact(string phoneNumber)
+    { contacts.RemoveAll(c => c.phoneNumber == phoneNumber); Save(); } // Delete a contact based on the number
+
+}
+
+// Ensure names and phone numbers stay paired up
+public class ContactInfo
+{
+    public string name;
+    public string phoneNumber;
+
+    public ContactInfo(string name, string phoneNumber)
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
